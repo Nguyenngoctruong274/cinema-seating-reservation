9ade5aed4b07201b9fadfbc21f3cbc9625fcb770
Nguyenngoctruong274 <ngoctruong274@gmail.com>
2025-06-25 11:57:28 +0700

    update api

--- service/handler/cinema_handler.go
+++ service/handler/cinema_handler.go
@@ -71,8 +71,11 @@ func (h *cinemaHandler) CheckAvailableSeats(c *gin.Context) {
 		return
 	}
 
-	data := h.CinemaUseCase.CheckAvailableSeats(req)
-
+	data, err := h.CinemaUseCase.CheckAvailableSeats(req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"data": data})
 }
 
